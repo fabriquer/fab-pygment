6049c8261f29b9e7e9e585f293eed8781893881e
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-09-22 14:09:03 -0230

    Convert tabs to spaces (PEP8 compliance).

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup (
-	name = 'fablexer',
-	packages = find_packages(),
-	entry_points =
-	"""
-	[pygments.lexers]
-	fablexer = fablexer.lexer:FabLexer
-	""",
+    name = 'fablexer',
+    packages = find_packages(),
+    entry_points =
+    """
+    [pygments.lexers]
+    fablexer = fablexer.lexer:FabLexer
+    """,
 )
